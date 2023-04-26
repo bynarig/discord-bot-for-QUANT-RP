610ec0769b2a68755d8a602f2d5d8c8e6a90dae4
Yaroslav Shabelnyk <shabelnyk63@gmail.com>
2023-04-26 10:22:09 +0300

    V.1
    Minimum work needs

---
+++ commands/dog-command.js
@@ -0,0 +1,20 @@
+module.exports = params = [
+  {
+    name: 'violencer',
+    description: 'ewd',
+    type: 3,
+    required: true,
+  },
+  {
+    name: 'type',
+    description: 'Parameter 2',
+    type: 3,
+    required: true,
+  },
+  {
+    name: 'reason',
+    description: 'Parameter 2',
+    type: 3,
+    required: true,
+  }
+];
